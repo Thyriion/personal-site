47f9f78218a59bfae58ce80ba366c6d06fdbb97c
Thyriion <kevinschueler2000@gmail.com>
2022-11-12 12:29:46 +0100

    Circle für Skills hinzugefügt

--- personal-site/components/Skills.tsx
+++ personal-site/components/Skills.tsx
@@ -1,35 +1,55 @@
 import React from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function Skills() {
-  const skills: { name: string }[] = [
+  const skills: { name: string; progress: number }[] = [
     {
       name: "C#",
+      progress: 45,
     },
     {
       name: "Delphi",
+      progress: 30,
     },
     {
       name: "HTML, CSS, JS",
+      progress: 80,
     },
     {
       name: "Wordpress",
+      progress: 75,
     },
     {
       name: "PHP",
+      progress: 85,
     },
     {
       name: "React",
+      progress: 60,
     },
     {
       name: "Tailwind",
+      progress: 50,
     },
   ];
+
   return (
     <div className="flex gap-10 flex-wrap justify-center items-center">
-      {skills.map((skill) => {
+      {skills.map((skill, index) => {
         return (
-          <div className="py-10 h-80 w-96 shadow-2xl px-10 animate-fade-in">
-            <h3>{skill.name}</h3>
+          <div
+            className="py-10 h-fit w-96 shadow-2xl px-10 animate-fade-in"
+            key={index}
+          >
+            <h3 className="pb-10">{skill.name}</h3>
+
+            <div className="h-fit">
+              <CircularProgressbar
+                value={skill.progress}
+                text={`${skill.progress}%`}
+              />
+            </div>
           </div>
         );
       })}
--- personal-site/components/Werdegang.tsx
+++ personal-site/components/Werdegang.tsx
@@ -1,7 +1,100 @@
 import React from "react";
 
 function Werdegang() {
-  return <div className="text-left"></div>;
+  return (
+    <div className="text-left">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        assumenda commodi ducimus eum fugit libero maxime necessitatibus nemo,
+        quia sequi. Culpa deserunt ducimus eos incidunt iure nemo odio velit
+        voluptate.
+      </p>
+    </div>
+  );
 }
 
 export default Werdegang;
