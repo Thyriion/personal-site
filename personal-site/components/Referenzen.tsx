b54cfd6e605d03ebe5a09f3f1102962f3c4f140a
Thyriion <kevinschueler2000@gmail.com>
2022-11-27 21:06:09 +0100

    Github Finder app hinzugefügt

--- personal-site/components/Referenzen.tsx
+++ personal-site/components/Referenzen.tsx
@@ -1,57 +1,62 @@
-import React from "react";
-import { FaGithub, FaChrome } from "react-icons/fa";
-import { GrGamepad } from "react-icons/gr";
+import React from 'react';
+import { FaGithub, FaChrome } from 'react-icons/fa';
+import { GrGamepad } from 'react-icons/gr';
 
 function Referenzen() {
-  return (
-    <div className="flex flex-col mx-auto w-fit">
-      <div className="w-fit text-2xl my-5">
-        <a
-          href="https://github.com/Thyriion"
-          target="_blank"
-          className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
-          rel="noreferrer"
-        >
-          <FaGithub />
-          Quellcode
-        </a>
-      </div>
-      <div className="w-fit text-2xl my-5">
-        <a
-          href="https://www.selectline.de/jobs/"
-          target="_blank"
-          className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
-          rel="noreferrer"
-        >
-          <FaChrome />
-          SelectLine Jobsseite
-        </a>
-      </div>
-      <div className="w-fit text-2xl my-5">
-        <a
-          href="https://hangman-thyriion.vercel.app/"
-          target="_blank"
-          className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
-          rel="noreferrer"
-        >
-          <GrGamepad />
-          Hangman mit React und Typescript
-        </a>
-      </div>
-
-      <div className="w-fit text-2xl my-5">
-        <a
-          href="https://feedback-ui-ks.netlify.app/"
-          target="_blank"
-          className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
-          rel="noreferrer"
-        >
-          <FaChrome />
-          Feedback App
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col mx-auto w-fit">
+            <div className="w-fit text-2xl my-5">
+                <a
+                    href="https://github.com/Thyriion"
+                    target="_blank"
+                    className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
+                    rel="noreferrer">
+                    <FaGithub />
+                    Quellcode
+                </a>
+            </div>
+            <div className="w-fit text-2xl my-5">
+                <a
+                    href="https://www.selectline.de/jobs/"
+                    target="_blank"
+                    className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
+                    rel="noreferrer">
+                    <FaChrome />
+                    SelectLine Jobsseite
+                </a>
+            </div>
+            <div className="w-fit text-2xl my-5">
+                <a
+                    href="https://hangman-thyriion.vercel.app/"
+                    target="_blank"
+                    className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
+                    rel="noreferrer">
+                    <GrGamepad />
+                    Hangman mit React und Typescript
+                </a>
+            </div>
+            <div className="w-fit text-2xl my-5">
+                <a
+                    href="https://feedback-ui-ks.netlify.app/"
+                    target="_blank"
+                    className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
+                    rel="noreferrer">
+                    <FaChrome />
+                    Feedback App
+                </a>
+            </div>
+            <div className="w-fit text-2xl my-5">
+                <a
+                    href="https://github-finder-app-one-ecru.vercel.app/"
+                    target="_blank"
+                    className="flex justify-center items-center gap-5 hover:scale-105 hover:text-gray-100"
+                    rel="noreferrer">
+                    <FaChrome />
+                    Github Finder App
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default Referenzen;
